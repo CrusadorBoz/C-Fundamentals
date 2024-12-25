a25f15683a6a68da4fb36468d32316ddf96ae1dc
Gordon <dowspirit@gmail.com>
2024-12-25 14:15:29 -0600

    Update Files

--- topics/functions.c
+++ topics/functions.c
@@ -12,10 +12,10 @@
 // //    // body of the function
 // // }
 
-// Return Type: This specifies the data type of the value that the function will return. If the function doesn't return anything, you use void.
-// Function Name: This is the name you give to the function, which follows C's naming rules (e.g., it should be descriptive).
-// Parameter List: These are variables that you pass to the function for it to work with. The parameters are optional; you can have a function 
-// with no parameters.
+// Return Type: This specifies the data type of the value that the function will return. If the function doesn't return 
+// anything, you use void.  Function Name: This is the name you give to the function, which follows C's naming rules 
+// (e.g., it should be descriptive).  Parameter List: These are variables that you pass to the function for it to work 
+// with. The parameters are optional; you can have a function with no parameters.
 
 // Body: Contains the code that the function executes when it's called.
 
@@ -39,23 +39,26 @@ int add(int a, int b) {
 }
 
 // Key Points:
-// Function Declaration: Before using a function, you should declare it, often at the top of the file or in a header if you're
-// splitting your code into multiple files. This tells the compiler what the function looks like.
+// Function Declaration: Before using a function, you should declare it, often at the top of the file 
+// or in a header if you're splitting your code into multiple files. This tells the compiler what the 
+// function looks like.
 
 // Function Definition: This is where you write the actual code for the function.
 
-// Calling a Function: Inside main() or any other function, you can call your function by its name followed by parentheses 
-// containing the arguments.
+// Calling a Function: Inside main() or any other function, you can call your function by its name 
+// followed by parentheses containing the arguments.
 
-// Return Values: Functions that return something must include a return statement with the value to return. If the function is 
-// void, you might not need a return statement, but it's good practice to include one with no value (just return;).
+// Return Values: Functions that return something must include a return statement with the value to 
+// return. If the function is void, you might not need a return statement, but it's good practice to 
+// include one with no value (just return;).
 
-// Scope: Variables declared inside a function are local to that function unless they are passed by reference or are global.
+// Scope: Variables declared inside a function are local to that function unless they are passed by 
+// reference or are global.
 
 // Tips for Using Functions:
 // Modularity: Break down your program into small, manageable pieces. 
 // Reusability: Write functions for operations you might need to use multiple times.
 // Testing: Functions make code easier to test since you can check each function's output individually.
 
-// If you have any specific questions about functions or if you want to see more examples on particular aspects like recursion, 
-// function pointers, or passing arrays to functions, feel free to ask!
+// If you have any specific questions about functions or if you want to see more examples on particular 
+// aspects like recursion, function pointers, or passing arrays to functions, feel free to ask!
